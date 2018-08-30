dc291cebff54044f2479844d2347564d6153c954
lakshmiobulasetti <41731019+lakshmiobulasetti@users.noreply.github.com>
2018-08-30 14:52:15 +0530

    Update and rename README.md to str.java

---
+++ str.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int n, a,count=0;
+        String x = "";
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter any decimal number:");
+        n = s.nextInt();
+        while(n > 0)
+        {
+            a = n % 2;
+            if(a == 1)
+            {
+                count++;
+            }
+            x = x + "" + a;
+            n = n / 2;
+        }
+        System.out.println("Binary number:"+x);
+    }
+}
